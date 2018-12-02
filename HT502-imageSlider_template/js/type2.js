bd68667fe5dda42984681eb18fd69e26e22ca48e
kimseokyung <kmgc18@naver.com>
2018-12-02 14:27:42 +0900

    t0000

--- HT502-imageSlider_template/js/type2.js
+++ HT502-imageSlider_template/js/type2.js
@@ -2,7 +2,11 @@ $(document).ready(function(){
     $("#slider ul li.item:gt(0)").hide();
 
     setInterval(function(){
-        $("#slider ul li.item:first").fadeOut(1000).next().fadeIn(1000).end().appendTo("#slider ul");
+        $("#slider ul li.item:first").fadeOut(1000)
+        .next()
+        .fadeIn(1000)
+        .end()
+        .appendTo("#slider ul");
 
     },2000);
 });
