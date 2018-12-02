322f6977e82d8e8ddb50d00fa821f0e26c09d204
kimseokyung <kmgc18@naver.com>
2018-12-02 14:52:10 +0900

    tes123456789

--- HT502-imageSlider_template/js/type3.js
+++ HT502-imageSlider_template/js/type3.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
-   
+    $("#slider ul li.item:gt(0)").hide();
+
+    var start;
+    var fading = function(){
+       start = setInterval(function(){
+            $("#slider ul li.item:first").fadeOut(1000)
+            .next()
+            .fadeIn(1000)
+            .end()
+            .appendTo("#slider ul");
+    
+        },2000);
+
+    }
+
+    fading();
+
+    $("#slider").hover(function(){
+        console.log("mouse in");
+        clearInterval(start);
+
+    },function(){
+        fading();
+        console.log("mouse out");
+
+    });
 });
